af01b0dde9d95c491c7926ca7872be03837ed7ae
HNBL <48491297+HNBL@users.noreply.github.com>
2023-11-16 18:07:49 +0300

    Add files via upload

---
+++ MyProgram.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+static void Main()
+{
+	string myName;
+	Console.WriteLine("Please enter your name");
+	myName=Console.ReadLine();
+	Console.WriteLine("Hell, {0}", myName);
+}
+	
+}
